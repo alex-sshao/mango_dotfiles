725fcb61a4b63b19ead634cdb74f94629c8d067d
Alex <alexs237044@gmail.com>
2026-06-30 10:37:30 -0400

    chore: temp comment

--- setup.sh
+++ setup.sh
@@ -14,3 +14,5 @@ link(){
 link mango ~/.config/mango
 
 
+# REQUIRED PACKAGES:
+# rofi xdg-desktop-portal-wlr swaybg waybar wl-clip-persist cliphist wl-clipboard wlsunset xfce-polkit swaync pamixer wlr-dpms sway-audio-idle-inhibit-git swayidle dimland-git brightnessctl swayosd wlr-randr grim slurp satty swaylock-effects-git wlogout sox
