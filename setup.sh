3cfbb7462841e6c52279b829a549edefdc6710ca
Alex <alexs237044@gmail.com>
2026-06-30 03:16:20 -0400

    Feat: Better setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-ln -sf "$(exec pwd)"/mango ~/.config/mango
+
+checkcmd(){
+	if [ $? -eq 0 ]; then
+		echo Success!
+	else
+		echo Fail!
+	fi
+}
+bd="$( cd "$( dirname "$0" )" && pwd )"
+echo ${bd}
+echo Linking ${bd} to ~/.config/mango... 
+ln -sf ${bd}/mango ~/.config/mango
+checkcmd
+
