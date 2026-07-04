c50b1aac21d632cd34b2cf5e0627ecb73e26054f
Alex <alexs237044@gmail.com>
2026-07-04 02:38:35 -0400

    Fix: remove programs_new

--- setup.sh
+++ setup.sh
@@ -15,4 +15,4 @@ link mango ~/.config/mango
 
 
 # REQUIRED PACKAGES:
-# rofi xdg-desktop-portal-wlr swaybg waybar wl-clip-persist cliphist wl-clipboard wlsunset xfce-polkit swaync pamixer wlr-dpms sway-audio-idle-inhibit-git swayidle dimland-git brightnessctl swayosd wlr-randr grim slurp satty swaylock-effects-git wlogout sox
+# rofi xdg-desktop-portal-wlr swaybg waybar wl-clip-persist cliphist wl-clipboard wlsunset xfce-polkit swaync pamixer wlr-dpms sway-audio-idle-inhibit-git swayidle dimland-git brightnessctl swayosd wlr-randr grim slurp satty swaylock-effects-git wlogout sox oxygen-cursors pavucontrol
