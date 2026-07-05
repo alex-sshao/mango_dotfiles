679730b452b01ba08484405f018c677efd043df4
Alex <alexs237044@gmail.com>
2026-07-05 02:39:28 -0400

    Feat: overwrite original config input option for setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 bd="$( cd "$( dirname "$0" )" && pwd )/"
-link(){
+
+
+linkf(){
+	if [ ! -f $2 ] || [ ! -d $2 ]; then 
+		echo "$2 found, do you want to replace it?"
+		read -p "->(Y/N) " conf
+		if [ $conf == [yY] ]; then
+			rm $2
+			echo "Removed $2, linking..."
+		else
+			echo "$2 files untouched."
+			return 0
+		fi
+	fi
 	echo "Linking ${bd}$1 to $2"
 	ln -sf ${bd}$1 $2
 	if [ $? -eq 0 ]; then
@@ -11,7 +24,7 @@ link(){
 	fi
 }
 
-link mango ~/.config/mango
+linkf mango ~/.config/mango
 
 
 # REQUIRED PACKAGES:
